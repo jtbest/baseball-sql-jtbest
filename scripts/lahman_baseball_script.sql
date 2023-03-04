e5d1d1bd5d4629e444c9bcd4346913b5ca498a0c
Jtbest <jtbest713@gmail.com>
2023-03-04 14:40:47 -0600

    11

--- scripts/lahman_baseball_script.sql
+++ scripts/lahman_baseball_script.sql
@@ -142,12 +142,12 @@ ORDER BY lifetime_salary DESC;
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
 SELECT 
-	CASE WHEN pos IN ('LF','RF','CF') THEN 'Outfield'
+	CASE WHEN pos IN ('LF','RF','CF','OF') THEN 'Outfield'
 		WHEN pos IN ('SS','1B','2B','3B') THEN 'Infield'
 		WHEN pos IN ('P','C') THEN 'Battery'
 		ELSE 'Not listed' END AS position_group, 
 	SUM(po) as total_putouts
-FROM fieldingpost
+FROM fielding
 WHERE yearid = '2016'
 GROUP BY position_group
 ORDER BY total_putouts DESC;
@@ -206,6 +206,7 @@ WHERE WSwin = 'N' AND yearid BETWEEN '1970' AND '2016' AND
 	w = (SELECT MAX(w)
 		  FROM teams as sub
 		  WHERE WSwin = 'N' AND yearid BETWEEN '1970' AND '2016')
+	  
 
 -- Seattle Mariners in 2001 won 116 games and didn't win WS.
 
@@ -248,23 +249,24 @@ WHERE wswin = 'Y'
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
-SELECT teams.park, name, homegames.attendance/homegames.games as avg_attd
+(SELECT teams.park, name, homegames.attendance/homegames.games as avg_attd, 'high' as class
 FROM homegames
 JOIN teams
 ON homegames.team = teams.teamid AND homegames.year = teams.yearid
 WHERE year = '2016' AND games >= 10
 ORDER BY avg_attd DESC 
-LIMIT 5; -- Top 5
+LIMIT 5)
 
+UNION
 
-SELECT teams.park, name, homegames.attendance/homegames.games as avg_attd
+(SELECT teams.park, name, homegames.attendance/homegames.games as avg_attd, 'low' as class
 FROM homegames
 JOIN teams
 ON homegames.team = teams.teamid AND homegames.year = teams.yearid
 WHERE year = '2016' AND games >= 10
 ORDER BY avg_attd  
-LIMIT 5; -- Bottom 5
-
+LIMIT 5) -- Bottom 5
+ORDER BY class
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
 
@@ -338,16 +340,16 @@ ORDER BY sub.name, year
 
 WITH cte as(
 			SELECT sub.name as name, year, team_name, league
-		FROM (SELECT playerid, yearid as year, CONCAT(p.namefirst, ' ', p.namelast) as name, awardid, COUNT(playerid) OVER (PARTITION BY 			playerid) as tsn_count
+		FROM (SELECT playerid, yearid as year, CONCAT(p.namefirst, ' ', p.namelast) as name, awardid, COUNT(playerid) OVER (PARTITION BY playerid) as tsn_count
 				FROM awardsmanagers as a
 				LEFT JOIN people as p
 				USING (playerid)
-			  WHERE awardid LIKE 'TSN%') as sub
+			  	WHERE awardid LIKE 'TSN%') as sub
 		LEFT JOIN (SELECT playerid, t.teamid, m.yearid, t.name as team_name, m.lgid as league
-				  FROM managers as m
-				  INNER JOIN teams as t
-				  ON m.teamid = t.teamid AND m.yearid = t.yearid
-				  GROUP BY playerid, m.yearid, t.teamid, t.name, m.lgid) as tsub
+				 	FROM managers as m
+				  	INNER JOIN teams as t
+				  	ON m.teamid = t.teamid AND m.yearid = t.yearid
+				  	GROUP BY playerid, m.yearid, t.teamid, t.name, m.lgid) as tsub
 			ON sub.playerid = tsub.playerid AND sub.year = tsub.yearid
 		WHERE tsn_count > 1) 
 		
@@ -362,11 +364,127 @@ ORDER BY name, year -- this one works
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
+SELECT *
+FROM batting
+WHERE yearid = '2016' AND hr > 0
+
+
+SELECT playerid, max_hr, yearid
+FROM (SELECT distinct playerid, MAX(hr) OVER (PARTITION BY playerid) as max_hr, yearid
+		FROM batting as sub
+	  Where playerid = 'abreujo02'
+		GROUP by playerid, yearid, hr
+		HAVING MAX(hr) = SELECT( MAX(hr) OVER (PARTITION BY playerid)
+	 FROM BATTING)) as hrs
+ORDER BY playerid
+
+
+SELECT playerid, yearid, MAX(hr)
+FROM batting
+Where playerid LIKE 'cano%'
+GROUP BY playerid, yearid
+
+-- Find what year was each player's max hr output
+SELECT playerid, yearid, hr
+FROM batting as b
+WHERE hr in (SELECT MAX(hr) OVER (PARTITION BY playerid)
+			FROM batting as sub
+			WHERE b.playerid = sub.playerid ) AND hr > 1
+			
+-- Use this as a cte to give all players with their max hr in 2016
+
+WITH cte as (SELECT playerid, yearid, hr
+FROM batting as b
+WHERE hr in (SELECT MAX(hr) OVER (PARTITION BY playerid)
+			FROM batting as sub
+			WHERE b.playerid = sub.playerid ) 
+	AND hr >= 1
+	AND playerid IN (SELECT playerid
+					FROM batting
+					GROUP BY playerid
+					HAVING COUNT( DISTINCT yearid) >= 10))
+			
+SELECT CONCAT(p.namefirst, ' ', p.namelast), hr
+FROM cte
+INNER JOIN people as p
+USING (playerid)
+WHERE yearid = '2016'
+ORDER BY hr DESC -- this doesn't accunt for mid-season trades
+
+SELECT *
+FROM batting
+WHERE playerid = 'colonba01'
+-- Find how many seasons each player has played. added above in cte
+SELECT playerid, COUNT(yearid)
+FROM batting
+GROUP BY playerid
+HAVING COUNT(yearid) >= 10
+--
+WITH cte as (SELECT playerid, yearid, hrs
+			FROM (SELECT playerid, yearid, SUM(hr) as hrs
+						FROM batting
+						GROUP BY playerid, yearid) as sub
+			WHERE hrs in (SELECT MAX(hrs) OVER (PARTITION BY playerid)
+								FROM (SELECT playerid, yearid, SUM(hr) as hrs
+										FROM batting as bb
+										GROUP BY playerid, yearid) as bsub
+								WHERE bsub.playerid = sub.playerid ) 
+				AND hrs >= 1
+				AND playerid IN (SELECT playerid
+									FROM batting
+									GROUP BY playerid
+									HAVING COUNT( DISTINCT yearid) >= 10))
+			
+SELECT CONCAT(p.namefirst, ' ', p.namelast), hrs
+FROM cte
+INNER JOIN people as p
+USING (playerid)
+WHERE yearid = '2016'
+ORDER BY hrs DESC -- this one
+--
+SELECT playerid, yearid, SUM(hr) as hrs
+FROM batting
+GROUP BY playerid, yearid
 
 -- **Open-ended questions**
 
 -- 11. Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question. As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
 
+WITH s as (SELECT s.yearid, s.teamid, SUM(s.salary) as total_sal
+				FROM salaries as s
+		  		GROUP BY s.teamid,s.yearid)
+
+SELECT s.yearid, s.teamid, total_sal,
+	CASE WHEN ((total_sal-AVG(total_sal) OVER (Partition by s.yearid)::numeric)/total_sal)::numeric > 0 THEN CONCAT('+',ROUND(((total_sal-AVG(total_sal) OVER (Partition by s.yearid)::numeric)/total_sal)::numeric,2)::varchar) ELSE ROUND(((total_sal-AVG(total_sal) OVER (Partition by s.yearid)::numeric)/total_sal)::numeric,2) END as salary_pct_diff, 
+	t.w as wins, 
+	RANK() OVER (PARTITION BY s.yearid order by total_sal DESC ) as sal_rank, 
+	RANK() OVER (PARTITION BY t.yearid ORDER BY t.w DESC) as win_rank
+FROM s
+LEFT JOIN teams as t
+	ON t.teamid=s.teamid AND s.yearid=t.yearid
+ORDER BY yearid DESC, win_rank
+
+
+WITH s as (SELECT s.yearid, s.teamid, SUM(s.salary) as total_sal
+				FROM salaries as s
+		  		GROUP BY s.teamid,s.yearid)
+
+SELECT s.yearid, s.teamid, total_sal,
+	ROUND(((total_sal-AVG(total_sal) OVER (Partition by s.yearid)::numeric)/total_sal)::numeric,2) as salary_pct_diff,
+	t.w as wins, 
+	RANK() OVER (PARTITION BY s.yearid order by total_sal DESC ) as sal_rank, 
+	RANK() OVER (PARTITION BY t.yearid ORDER BY t.w DESC) as win_rank
+FROM s
+LEFT JOIN teams as t
+	ON t.teamid=s.teamid AND s.yearid=t.yearid
+ORDER BY yearid DESC, win_rank
+
+-- 
+
+
+
+SELECT *
+FROM Salaries
 -- 12. In this question, you will explore the connection between number of wins and attendance.
 --     <ol type="a">
 --       <li>Does there appear to be any correlation between attendance at home games and number of wins? </li>
@@ -378,3 +496,46 @@ ORDER BY name, year -- this one works
 
   
 
+-- ## Question 1: Rankings
+-- #### Question 1a: Warmup Question
+-- Write a query which retrieves each teamid and number of wins (w) for the 2016 season. Apply three window functions to the number of wins (ordered in descending order) - ROW_NUMBER, RANK, AND DENSE_RANK. Compare the output from these three functions. What do you notice?
+-- ​
+-- #### Question 1b: 
+-- Which team has finished in last place in its division (i.e. with the least number of wins) the most number of times? A team's division is indicated by the divid column in the teams table.
+-- ​
+-- ## Question 2: Cumulative Sums
+-- #### Question 2a: 
+-- Barry Bonds has the record for the highest career home runs, with 762. Write a query which returns, for each season of Bonds' career the total number of seasons he had played and his total career home runs at the end of that season. (Barry Bonds' playerid is bondsba01.)
+-- ​
+-- #### Question 2b:
+-- How many players at the end of the 2016 season were on pace to beat Barry Bonds' record? For this question, we will consider a player to be on pace to beat Bonds' record if they have more home runs than Barry Bonds had the same number of seasons into his career. 
+-- ​
+-- #### Question 2c: 
+-- Were there any players who 20 years into their career who had hit more home runs at that point into their career than Barry Bonds had hit 20 years into his career? 
+-- ​
+-- ## Question 3: Anomalous Seasons
+-- Find the player who had the most anomalous season in terms of number of home runs hit. To do this, find the player who has the largest gap between the number of home runs hit in a season and the 5-year moving average number of home runs if we consider the 5-year window centered at that year (the window should include that year, the two years prior and the two years after).
+-- ​
+-- ## Question 4: Players Playing for one Team
+-- For this question, we'll just consider players that appear in the batting table.
+-- #### Question 4a: 
+-- Warmup: How many players played at least 10 years in the league and played for exactly one team? (For this question, exclude any players who played in the 2016 season). Who had the longest career with a single team? (You can probably answer this question without needing to use a window function.)
+-- ​
+-- #### Question 4b: 
+-- Some players start and end their careers with the same team but play for other teams in between. For example, Barry Zito started his career with the Oakland Athletics, moved to the San Francisco Giants for 7 seasons before returning to the Oakland Athletics for his final season. How many players played at least 10 years in the league and start and end their careers with the same team but played for at least one other team during their career? For this question, exclude any players who played in the 2016 season.
+-- ​
+-- ## Question 5: Streaks
+-- #### Question 5a: 
+-- How many times did a team win the World Series in consecutive years?
+-- ​
+-- #### Question 5b: 
+-- What is the longest steak of a team winning the World Series? Write a query that produces this result rather than scanning the output of your previous answer.
+-- ​
+-- #### Question 5c: 
+-- A team made the playoffs in a year if either divwin, wcwin, or lgwin will are equal to 'Y'. Which team has the longest streak of making the playoffs? 
+-- ​
+-- #### Question 5d: 
+-- The 1994 season was shortened due to a strike. If we don't count a streak as being broken by this season, does this change your answer for the previous part?
+-- ​
+-- ## Question 6: Manager Effectiveness
+-- Which manager had the most positive effect on a team's winning percentage? To determine this, calculate the average winning percentage in the three years before the manager's first full season and compare it to the average winning percentage for that manager's 2nd through 4th full season. Consider only managers who managed at least 4 full years at the new team and teams that had been in existence for at least 3 years prior to the manager's first full season.
